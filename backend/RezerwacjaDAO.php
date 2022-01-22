9c42d27f8593bb929a9e812c64a676bdb5f95775
Gar2d2 <pghost286@gmail.com>
2022-01-22 13:38:19 +0100

    pobranie rezerwacji dla dnia

--- backend/RezerwacjaDAO.php
+++ backend/RezerwacjaDAO.php
@@ -1,6 +1,8 @@
 <?php
     include_once 'backend\RezerwacjaDTO.php';
     include_once 'backend\DBConnector.php';
+    include_once 'backend\PrzejazdDAO.php';
+
     class RezerwacjaDAO
     {
         
@@ -24,6 +26,39 @@
                 }
             }
             return $rezerwacje;
+        }
+
+        public function pobierzRezerwacjeZData($Data)
+        {
+            $connection = new DBConnector();
+            $rezerwacje[] = new RezerwacjaDTO();
+            $przejazdyDAO = new PrzejazdDAO();
+
+            $przejazdy = $przejazdyDAO->pobierzPrzejazdyZData($Data);
+            $j=0;
+            for($i=0;$i<count($przejazdy);$i++)
+            {
+               $rezerwacjePrzejazdu = $this->pobierzRezerwacjeDlaPrzejazdu($przejazdy[$i]->id);
+               for($k=0; $k<count($rezerwacjePrzejazdu); $k++)
+               {
+                    $rezerwacje[$j] = new RezerwacjaDTO();
+                    $rezerwacje[$j]->id = $rezerwacjePrzejazdu[$k]->id;
+                    $rezerwacje[$j]->idOsoby = $rezerwacjePrzejazdu[$k]->idOsoby;
+                    $rezerwacje[$j]->idPrzejazdu = $rezerwacjePrzejazdu[$k]->idPrzejazdu;
+                    $rezerwacje[$j]->potwierdzono = $rezerwacjePrzejazdu[$k]->potwierdzono;
+                    $j++;
+               }
+            }
+            return $rezerwacje;
+        }
+
+        public function potwierdzRezerwacje($Id)//tutaj DTO przejazd
+        {
+            $connection = new DBConnector();
+            //"UPDATE `przejazd` SET `dataPrzejazdu` = '$przejazdDTO->data'
+            $query="UPDATE `rezerwacja` SET potwierdzono = '1' WHERE id = $Id ";
+            $result = mysqli_query($connection->GetBazaConnection(), $query);
+            return $result;
         }
 
         public function dodajRezerwacje($rezerwacjaDTO)//tutaj DTO przejazd
